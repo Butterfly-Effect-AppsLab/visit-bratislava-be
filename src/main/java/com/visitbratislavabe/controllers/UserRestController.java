db3246d3793bf9fcb5b6d0fd3669deaea4640a57
Jozef Kysel <jojo.kysel00@gmail.com>
2020-03-08 18:09:45 +0100

    feature(json-parser): add jackson as json parser, and delete userDto class

--- src/main/java/com/visitbratislavabe/controllers/UserRestController.java
+++ src/main/java/com/visitbratislavabe/controllers/UserRestController.java
@@ -1,7 +1,6 @@
 package com.visitbratislavabe.controllers;
 
 import com.visitbratislavabe.models.User;
-import com.visitbratislavabe.utils.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +12,8 @@ import java.util.UUID;
 public class UserRestController {
 
 	@GetMapping("/me")
-	public UserDto currentUser() {
-		User user = new User(UUID.randomUUID(), "mockEmail", "mockName", "mockLastname");
-		return UserDto.fromUser(user);
+	public User currentUser() {
+		return new User(UUID.randomUUID(), "mockEmail", "mockName", "mockLastname");
 	}
 
 }
