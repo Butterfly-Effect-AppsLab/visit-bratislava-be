d0637a794caf5ce6e815e9e720755417dbc70f34
Jozef Kysel <jojo.kysel00@gmail.com>
2020-04-04 16:22:05 +0200

    feature(add-places): add endpoint and service to store places

--- src/main/java/com/visitbratislavabe/dbmodels/PlaceDto.java
+++ src/main/java/com/visitbratislavabe/dbmodels/PlaceDto.java
@@ -1,12 +1,10 @@
 package com.visitbratislavabe.dbmodels;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Map;
 import java.util.Set;
 
 @Entity
@@ -35,26 +33,9 @@ public class PlaceDto {
 
 	private String image;
 
-	@JsonProperty("location_string")
 	private String locationString;
 
-	@JsonProperty("ranking_category")
 	private String rankingCategory;
-
-	@Transient
-	private Map<String, Object> imagesObject;
-
-	@Transient
-	private Map<String, Object> originalImage;
-
-	// TODO: this part will be removed when database will be created
-	@SuppressWarnings("unchecked")
-	@JsonProperty("photo")
-	private void unpackNested(Map<String, Object> photo) {
-		this.imagesObject = (Map<String, Object>) photo.get("images");
-		this.originalImage = (Map<String, Object>) imagesObject.get("original");
-		this.image = (String) originalImage.get("url");
-	};
 
 	public PlaceDto() {
 	}
