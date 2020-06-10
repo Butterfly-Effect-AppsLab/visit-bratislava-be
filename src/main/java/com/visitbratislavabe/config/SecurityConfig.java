2f19e43119369dfef6ee7eda3488a547affdcaf8
RandomUserIK <xkovaci1@stuba.sk>
2020-06-10 18:47:42 +0200

    feature(login): configure login logic

--- src/main/java/com/visitbratislavabe/config/SecurityConfig.java
+++ src/main/java/com/visitbratislavabe/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.visitbratislavabe.config;
 
-import com.visitbratislavabe.handlers.FailureHandler;
 import com.visitbratislavabe.handlers.SuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -15,13 +20,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private SuccessHandler successHandler;
 
-	@Autowired
-	private FailureHandler failureHandler;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest()
-				.authenticated().and().oauth2Login().successHandler(successHandler).failureHandler(failureHandler);
+		http
+				.cors().and().antMatcher("/**").authorizeRequests().antMatchers("/").permitAll()
+				.antMatchers("/oauth2/**").permitAll().anyRequest()
+				.authenticated().and().oauth2Login().successHandler(successHandler).failureUrl("/");
+
+		// ! TODO: add defaultSuccessUrl("/profile")
+	}
+
+	@Bean
+	protected CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type",
+													"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
--- src/main/java/com/visitbratislavabe/handlers/SuccessHandler.java
+++ src/main/java/com/visitbratislavabe/handlers/SuccessHandler.java
@@ -5,7 +5,11 @@ import com.visitbratislavabe.services.UserRepositoryService;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -20,25 +24,17 @@ public class SuccessHandler implements AuthenticationSuccessHandler {
 	@Autowired
 	private UserRepositoryService userRepository;
 
-//	 @Autowired
-//	 private ObjectMapper objectMapper;
-
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 
-		DefaultOidcUser userDetails = (DefaultOidcUser) authentication.getPrincipal();
-		if(userRepository.getByEmail(userDetails.getEmail()) != null) {
+		DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
+		if(userRepository.getByEmail((String) userDetails.getAttributes().get("email")) != null) {
 			User newUser = new User();
-			newUser.setName(userDetails.getFullName());
-			newUser.setEmail(userDetails.getEmail());
-			newUser.setImageUrl(userDetails.getPicture());
+			newUser.setName((String) userDetails.getAttributes().get("name"));
+			newUser.setEmail((String) userDetails.getAttributes().get("email"));
 			userRepository.save(newUser);
 		}
-
-//		 response.getWriter().write(objectMapper.writeValueAsString(authInfo));
-//		 response.setContentType("application/json");
-//		 response.setStatus(200);
 	}
 
 }
